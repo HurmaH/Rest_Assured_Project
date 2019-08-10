efa42eabada263dfa7c1ae9b46c9268a4d5de526
Hurma H <hurmahm@gmail.com>
2019-08-10 14:51:37 -0500

    Base URI

--- src/test/java/com/app/tests/BaseURI.java
+++ src/test/java/com/app/tests/BaseURI.java
@@ -1,4 +1,39 @@
 package com.app.tests;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class BaseURI {
+
+    @BeforeClass
+    public void setUpClass(){
+        //baseURI --> used to save the base url for all resources
+        //when we actually make requests,
+        // we only provide the path to specific resource
+        RestAssured.baseURI = "https://api.got.show/api";
+    }
+
+
+    @Test
+    public void culturesTest(){
+
+       Response response = RestAssured.get("/cultures/");
+       response.prettyPrint();
+
+        Assert.assertTrue(response.asString().contains("Ironborn"));
+
+    }
+
+    @Test
+    public void citiesTest() {
+
+        Response response = RestAssured.get("/cities/");
+        response.prettyPrint();
+
+        Assert.assertTrue(response.asString().contains("White Harbor"));
+    }
+
 }
