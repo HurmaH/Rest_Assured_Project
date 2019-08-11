441ae0ee0c5b4890318de43c6fe89868deb885b9
Hurma H <hurmahm@gmail.com>
2019-08-11 04:32:27 -0500

    Satement 2

--- src/test/java/com/app/tests/ParamState.java
+++ src/test/java/com/app/tests/ParamState.java
@@ -20,10 +20,29 @@ public class ParamState{
        //given() --> statement where we prepare the request
         // when() --> send request
         given().
-                pathParam("name", "Qarth").//adding param variable name, value Qarth
+                pathParam("name", "Braavos").//adding param variable name, value Qarth
 
         when().
                 get("/map/cities/{name}").//{name} will be replaced by value of the path param
+                prettyPrint();
+    }
+
+
+    @Test
+    public void getCharacterByName(){
+        given().
+                pathParam("name", "Pypar").
+        when().
+                get("/map/characters/{name}").
+                prettyPrint();
+    }
+
+    @Test
+    public void getCharacterById(){
+        given().
+                pathParam("id", "5cc0743604e71a0010b854cd").
+        when().
+                get("map/characters/byId/{id}"). //https://api.got.show/api/map/characters/byId/:id
                 prettyPrint();
     }
 }
